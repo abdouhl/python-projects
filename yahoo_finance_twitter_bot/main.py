b4c9d0a6aef9c424f94afba2e45d952d86f163ce
Abdou <abdou@abdou.com>
2022-06-21 00:17:38 +0100

    abdouhl

--- yahoo_finance_twitter_bot/main.py
+++ yahoo_finance_twitter_bot/main.py
@@ -12,10 +12,8 @@ load_dotenv(join(dirname(__file__), '.env'))
 
 req = requests.get('https://finance.yahoo.com/news/rssindex')
 bs = BeautifulSoup(req.text, 'xml')
-item = bs.find('item')
-title = item.find('title').text
-link = item.find('link').text
-image_url = item.find('media:content').attrs['url']
+items = bs.find_all('item')
+
 print(join(dirname(__file__), '.env'))
 print(os.environ.get("YAHOO_FINANCE_TWITTER_BOT_CONSUMER_KEY"))
 
@@ -27,13 +25,28 @@ twitter_auth_keys = {
 }
 
 auth = tweepy.OAuthHandler(
-        twitter_auth_keys['consumer_key'],
-        twitter_auth_keys['consumer_secret']
-        )
+    twitter_auth_keys['consumer_key'],
+    twitter_auth_keys['consumer_secret']
+    )
 auth.set_access_token(
-        twitter_auth_keys['access_token'],
-        twitter_auth_keys['access_token_secret']
-        )
+    twitter_auth_keys['access_token'],
+    twitter_auth_keys['access_token_secret']
+    )
 api = tweepy.API(auth)
 
-print(api.get_user(screen_name = 'jurad0x').name)
+
+last_tweet_title = api.user_timeline(count=1)[0].text
+
+items.reverse()
+try:
+    tweet_index = items.index(last_tweet_title)
+except:
+    tweet_index = 0
+
+
+title = items[tweet_index].find('title').text
+#link = item.find('link').text
+#image_url = item.find('media:content').attrs['url']
+    
+
+api.update_status(title[:280])
