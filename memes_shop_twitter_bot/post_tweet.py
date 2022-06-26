bd170f9c259fd821e51ca9c1cac1d0551bd908ea
Abdou <abdou@abdou.com>
2022-06-26 14:16:38 +0100

    remove comments

--- memes_shop_twitter_bot/post_tweet.py
+++ memes_shop_twitter_bot/post_tweet.py
@@ -49,11 +49,10 @@ opener=urllib.request.build_opener()
 opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
 urllib.request.install_opener(opener)
 
-# setting filename and image URL
+
 filename = join(dirname(__file__), 'img.jpg')
 
 
-# calling urlretrieve function to get resource
 urllib.request.urlretrieve(img_url, filename)
 
 media = api.media_upload(filename)
