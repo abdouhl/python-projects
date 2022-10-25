2e378ae91fa9898fb65f621fda9c98b881165454
abderrahmane <73205653+abdouhl@users.noreply.github.com>
2022-10-25 17:04:32 +0100

    Update create_post.py

--- quotes_linkedin_bot/create_post.py
+++ quotes_linkedin_bot/create_post.py
@@ -5,6 +5,8 @@ import requests
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import urllib.request
+
 
 load_dotenv(join(dirname(__file__), '.env'))
 
@@ -19,7 +21,7 @@ headers = {'X-Restli-Protocol-Version': '2.0.0',
            'Authorization': f'Bearer {access_token}'}
 
 
-ch = random.choice(range(1,2))
+ch = random.choice(range(2,3))
 if ch == 1:
     interies = os.listdir('resources/quotes/')
     all_author_quotes = []
@@ -39,18 +41,6 @@ if ch == 1:
     author_tag = author_tag.replace('-','')
     author_tag = author_tag.replace("'",'')
     text = f'"{author_quote}" -- {author_name.title()}\n #{author_tag} #quotes #quotesandsayings #motivation #inspiration #sayings #quote #quoteoftheday'
-else:
-    with open('quotes_twiter_bot/quotes.json') as f:
-        quotes = json.load(f)
-         
-    quote_link = random.choice(list(quotes.keys()))
-    quote_text = quotes[quote_link][1]
-    author_name = quotes[quote_link][0]
-    auth_tag = author_name.replace(' ','').lower()
-    text = f'#{auth_tag} #quotes #quotesandsayings #motivation #inspiration #sayings #quote #quoteoftheday {quote_link}'
-    
-
-def post_on_linkedin():
     api_url = f'{api_url_base}ugcPosts'
 
     post_data = {
@@ -70,13 +60,89 @@ def post_on_linkedin():
     }
 
     response = requests.post(api_url, headers=headers, json=post_data)
+    if response.status_code == 201:
+        print("Success")
+        print(response.content)
+    else:
+        print(response.content)
+else:
+    with open('quotes_twiter_bot/quotes.json') as f:
+        quotes = json.load(f)
+         
+    quote_link = random.choice(list(quotes.keys()))
+    quote_text = quotes[quote_link][1]
+    author_name = quotes[quote_link][0]
+    auth_tag = author_name.replace(' ','').lower()
+    text_tags = f'#{auth_tag} #quotes #quotesandsayings #motivation #inspiration #sayings #quote #quoteoftheday'
+    
+    api_url = 'https://api.linkedin.com/v2/assets?action=registerUpload'
+    
+    post_data = {
+       "registerUploadRequest":{
+          "owner":URN,
+          "recipes":[
+             "urn:li:digitalmediaRecipe:feedshare-image"
+          ],
+          "serviceRelationships":[
+             {
+                "identifier":"urn:li:userGeneratedContent",
+                "relationshipType":"OWNER"
+             }
+          ],
+          "supportedUploadMechanism":[
+             "SYNCHRONOUS_UPLOAD"
+          ]
+       }
+    }
+    
+    response = requests.post(api_url, headers=headers, json=post_data)
+    
+    uploadurl = response.json()['value']['uploadMechanism']['com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest']['uploadUrl']
+    # "urn:li:image:C4D22AQEWUEuVbYrmAg"  "urn:li:digitalmediaAsset:C5522AQHn46pwH96hxQ"
+    imageurn = response.json()['value']['asset'].replace('digitalmediaAsset','image')
+    opener=urllib.request.build_opener()
+    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+    urllib.request.install_opener(opener)
+
+    # setting filename and image URL 
+    filename =  join(dirname(__file__),'og_card_image.jpg')
+    product_img_url = quote_link + 'og_card_image.jpg'
+
+    # calling urlretrieve function to get resource
+    urllib.request.urlretrieve(product_img_url, filename)
+
+    os.system(f'''curl -i --upload-file {join(dirname(__file__),'og_card_image.jpg')} -H 'Authorization: Bearer {access_token}' "{uploadurl}"''')
+    
+    api_url = 'https://api.linkedin.com/v2/posts'
+
+    post_data = {
+      "author": URN,
+      "commentary": text_tags,
+      "content": {
+            "article": {
+                "title": quote_text,
+                "source": quote_link,
+                "thumbnail": imageurn
+            }
+        },
+      "contentLandingPage":quote_link,
+       "visibility": "PUBLIC",
+       "distribution": {
+           "feedDistribution": "MAIN_FEED",
+           "targetEntities": [],
+           "thirdPartyDistributionChannels": []
+       },
+       "lifecycleState": "PUBLISHED",
+       "isReshareDisabledByAuthor": False
+    }
+
+    response = requests.post(api_url, headers=headers, json=post_data)
 
     if response.status_code == 201:
         print("Success")
         print(response.content)
     else:
         print(response.content)
-post_on_linkedin()
 
 
 
