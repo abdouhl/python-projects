2e7b2da3a7d1e4a48ec9a533c045aafebaa5873d
abderrahmane <73205653+abdouhl@users.noreply.github.com>
2022-11-02 13:41:26 +0100

    Update upload_quote.py

--- quotes_tumblr_bot/upload_quote.py
+++ quotes_tumblr_bot/upload_quote.py
@@ -1,82 +1,26 @@
 import json
 import os
 import random
-import pytumblr
-from os import listdir, remove
-from PIL import Image, ImageEnhance,ImageDraw,ImageFont
+import socket
 import sys
+import time
+from os import listdir, remove
+import praw 
+from praw.models import InlineImage
 from os.path import join, dirname
-import urllib.request
+from dotenv import load_dotenv
+import pygsheets
 
-# Authenticate via OAuth 
-client = pytumblr.TumblrRestClient(os.environ.get("QUOTES_TUMBLR_BOT_CONSUMER_KEY"),os.environ.get("QUOTES_TUMBLR_BOT_CONSUMER_SECRET"),os.environ.get("QUOTES_TUMBLR_BOT_OAUTH_TOKEN"),os.environ.get("QUOTES_TUMBLR_BOT_OAUTH_SECRET")) 
+time.sleep(60)
+load_dotenv(join(dirname(__file__), '.env'))
 
-# Make the request 
-
-def create_post():
-    enteries = os.listdir('resources/quotes/') 
-    quotes_len = 0
-    while quotes_len <= 20:
-        quote_file = random.choice(enteries)
-        print(quote_file)
-        with open(f'resources/quotes/{quote_file}') as f:
-            quote_file_content =  json.load(f)
-        if quote_file_content =={}:
-            continue
-        author_name = random.choice(list(quote_file_content.keys()))
-        all_author_quotes = quote_file_content[author_name]
-        quotes_len = len(all_author_quotes)
-    all_author_quotes = all_author_quotes[:20]
-
-    aauthor_name = author_name
-    auth = author_name.replace(',','')
-    auth = auth.replace('.','')
-    auth = auth.replace("'",'')
-    auth = auth.replace(" ",'-')
-    auth = auth.lower()
-    site_url = f"https://quotesandsayings.net/{auth}-quotes/"
-    title =f"The Best 20 {author_name.title()} Quotes"
-    slug = f"{auth}-quotes"
-    content = "<ol>"
-    k = 1
-    for quote in all_author_quotes:
-        content += f"<li>{quote}</li>"
-        if k ==5:
-            content +="</ol>"
-            content +=f'<h4>for more {author_name.title()} quotes:<a href="{site_url}">Click Here</a></h4>'
-            content +="[[MORE]]"
-            content +='<ol start="6" >'
-        k+=1
-    content +="</ol>"
-    tags=["quotes", "sayings","relatable quotes","relationship", "love quotes","love","quotes", "relationship quotes","life quotes","feelings", "quote","emotions"]
-    res = client.create_text('quotesandsayings-net',tags=tags, format='html',state="published", slug=slug, title=title, body=content)
-    print(res)
-    print(aauthor_name)
+reddit = praw.Reddit(client_id=os.environ.get("QUOTES_REDDIT_BOT_CLIENT_ID"),
+                     client_secret=os.environ.get("QUOTES_REDDIT_BOT_CLIENT_SECRET"),
+                     user_agent=os.environ.get("QUOTES_REDDIT_BOT_USER_AGENT"),
+                     redirect_uri=os.environ.get("QUOTES_REDDIT_BOT_REDIRECT_URI"),
+                     refresh_token=os.environ.get("QUOTES_REDDIT_BOT_REFRESH_TOKEN"))
 
 
-def create_card():
-    with open('quotes_twiter_bot/quotes.json') as f:
-        quotes = json.load(f)
-    quote_link = random.choice(list(quotes.keys()))
-    quote_text = quotes[quote_link][1]
-    author_name = quotes[quote_link][0]
-    auth_tag = author_name.replace(' ','').lower()
-    tags=[f"{auth_tag}","quotes", "sayings","relatable quotes","relationship", "love quotes","love","quotes", "relationship quotes","life quotes","feelings", "quote","emotions"]
-    opener=urllib.request.build_opener()
-    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-    urllib.request.install_opener(opener)
-
-    # setting filename and image URL 
-    filename =  join(dirname(__file__),'og_card_image.jpg')
-    image = quote_link + 'og_card_image.jpg'
-
-    # calling urlretrieve function to get resource
-    urllib.request.urlretrieve(image, filename)
-    client.create_photo('quotesandsayings-net', state="published", tags=tags,
-                    tweet=quote_text,
-                    caption=f""f'<a href="{quote_link}">{author_name.title()} quote:{quote_text}</a>',
-                    data=filename)
-    
 
 def create_quote():
     interies = os.listdir('resources/quotes/')
@@ -96,116 +40,99 @@ def create_quote():
             all_author_quotes = []    
     print(author_name)
     print(author_quote)
+
+    reddit_quote = f'"{author_quote}" -- {author_name.title()}\n#quotes #sayings #quotesandsayings'
+
+
     aauthor_name = author_name
     auth = author_name.replace(',','')
-    auth = auth.replace('.','')
-    auth = auth.replace("'",'')
-    auth = auth.replace(" ",'')
-    auth = auth.lower()
-    tags=[f"{auth}","quotes", "sayings","relatable quotes","relationship", "love quotes","love","quotes", "relationship quotes","life quotes","feelings", "quote","emotions"]
-    source =f"- {author_name} -"
-    res = client.create_quote('quotesandsayings-net',tags=tags, state="published", quote=author_quote, source=source)
- 
-
-
-def create_image_link():
-    quotes = []
-    images = []
-    interies = listdir('resources/images/')
-    if interies == []:
-        sys.exit()
-    for inter in interies:
-        if inter.endswith(".jpg"):
-            images.append(inter)
-
-    while quotes == [] :
-        image_path = random.choice(images)
-        author = image_path.replace('.jpg','')
-        author = author.replace('_',' ')
-        author = author.lower()
-        print(author)
-        print(image_path)
-        first_let = str(author).lower()[0]
-        print(first_let)
-        with open(f'resources/quotes/{first_let}_authors.json', 'r') as f:
-            aaaaa = json.load(f)
-        
-        quotes = aaaaa.get(author.lower())
-        
-        quote = random.choice(quotes)
-            
-            
-
-    print(quote)
-    auth_tag = author.replace(' ','').lower()
-    title_quote = f"{quote}"
-
-
-
-    with Image.open(f"resources/images/{image_path}") as im:
-        draw = ImageDraw.Draw(im)
-        
-        fff = 51
-        hig=1400
-        while hig >800:
-            txt = f'"{quote}"'
-            fff -= 1
-            fnt = ImageFont.truetype('resources/fonts/Fraunces/static/Fraunces/Fraunces-MediumItalic.ttf', fff)
-            wid,hig =draw.multiline_textsize( txt, font=fnt, spacing=20)
-            pkk = int(wid/900)+1
-            lenn = len(txt)
-            index = int(lenn/pkk)
-            txt = list(txt)
-            while index <lenn:
-                while txt[index] != ' ':
-                    index -=1
-                txt[index] = "\n"
-                index += int(lenn/pkk)
-            txt = "".join(txt)
-            print(txt)
-            
-            wid,hig =draw.multiline_textsize( txt, font=fnt, spacing=20)
-        draw.multiline_text((540, 450), txt, font=fnt,anchor="mm",align="center",spacing=20, fill="white")
-        fnt = ImageFont.truetype("resources/fonts/Tajawal-Bold.ttf", 35)
-        draw.text((540, (450+hig/2+100)), f'»»—— {author.title()} ——««', font=fnt,anchor="mb",align="center",spacing=20, fill="white")
-        fnt = ImageFont.truetype("resources/fonts/Tajawal-Bold.ttf", 25)
-        draw.text((540, (450+hig/2+200)), '– quotesandsayings.net', font=fnt,anchor="mb",align="center",spacing=20, fill="white")
-
-        im.save(join(dirname(__file__), 'img.jpg'))
-
-
-
-
-
-    title = title_quote
-    image = join(dirname(__file__), 'img.jpg')
-    
-    
-    auth = author.replace(',','')
-    auth = auth.replace('.','')
-    auth = auth.replace("'",'')
-    auth = auth.replace(" ",'')
-    auth = auth.lower()
-    tags=[f"{auth}","quotes", "sayings","relatable quotes","relationship", "love quotes","love","quotes", "relationship quotes","life quotes","feelings", "quote","emotions"]
-    
-    auth = author.replace(',','')
     auth = auth.replace('.','')
     auth = auth.replace("'",'')
     auth = auth.replace(" ",'-')
     auth = auth.lower()
     site_url = f"https://quotesandsayings.net/{auth}-quotes/"
-    client.create_photo('quotesandsayings-net', state="published", tags=tags,
-                    tweet=title_quote,
-                    caption=f""f'<a href="{site_url}">{author.title()} quote:{title}</a>',
-                    data=image)
-      
 
 
 
+    subr = 'quotesandsayings__net' 
+
+    subreddit = reddit.subreddit(subr) # Initialize the subreddit to a variable
+
+    choices = list(subreddit.flair.link_templates.user_selectable())
+   
+    template_id = next(x for x in choices if x["flair_text"] == "quote")["flair_template_id"] #quote image | quote
+    
+
+    reddit.validate_on_submit = True
+    submission = subreddit.submit(reddit_quote,flair_id=template_id,url=site_url)
+    submission.mod.approve()
+ 
+
+with open( join(dirname(__file__), 'gsheets_api-credentials.json'),'w') as f:
+    json.dump(json.loads(os.environ.get("QUOTES_REDDIT_BOT_GDRIVE_API_CREDENTIALS")),f)
+
+gc = pygsheets.authorize(service_file=join(dirname(__file__), 'gsheets_api-credentials.json'))
+
+sht = gc.open_by_key(os.environ.get("QUOTES_REDDIT_BOT_SHEET_KEY"))
+wks_users = sht.worksheet_by_title("users")
+
+done_users = []
+for number in range(1,100):
+    done_user = wks_users.cell(f'A{number}').value
+    if done_user == '':
+        break
+    done_users.append(done_user)
+
+def invetation(related_subreddits):
+    pk = 1
+    for submission in reddit.subreddit(related_subreddits).new(limit=400):
+        
+        if pk == 5:
+            break
+        if str(submission.author) not in done_users:
+            try:
+                print(str(submission.author))
+                pk += 1
+                reddit.redditor(str(submission.author)).message(
+                "the best quotes community", "I've invited you to join our community r/quotesandsayings__net", from_subreddit="quotesandsayings__net")
+                done_users
+                wks_users.cell(f'A{len(done_users)+1}').value = str(submission.author)
+                done_users.append(str(submission.author))
+            except:
+                continue
 
 
 
-
+def create_card():
+    with open('quotes_twiter_bot/quotes.json') as f:
+        quotes = json.load(f)
+    quote_link = random.choice(list(quotes.keys()))
+    quote_text = quotes[quote_link][1]
+    author_name = quotes[quote_link][0]
+    auth_tag = author_name.replace(' ','').lower()
+    tags=[f"{auth_tag}","quotes", "sayings","relatable quotes","relationship", "love quotes","love","quotes", "relationship quotes","life quotes","feelings", "quote","emotions"]
+    opener=urllib.request.build_opener()
+    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+    urllib.request.install_opener(opener)
+    # setting filename and image URL 
+    filename =  join(dirname(__file__),'og_card_image.jpg')
+    image = quote_link + 'og_card_image.jpg'
+    # calling urlretrieve function to get resource
+    urllib.request.urlretrieve(image, filename)
+    images = [
+        {
+            "image_path": image,
+            "caption": "➡️Click Here To Read More Quotes!➡️➡️",
+            "outbound_url": quote_link,
+        },
+        {
+            "image_path": image,
+            "caption": "➡️Click Here To Read More Quotes!➡️➡️",
+            "outbound_url": quote_link,
+        },
+    ]
+    submission = subreddit.submit_gallery(quote_text, images)
+    submission.mod.approve()
 
 number = random.randint(1,3)
 
@@ -215,3 +142,5 @@ elif number == 2:
     create_card()
 else:
     create_card()
+
+#invetation("Showerthoughts+im14andthisisdeep+quotes+QuotesPorn+Stoicism+GetMotivated")
